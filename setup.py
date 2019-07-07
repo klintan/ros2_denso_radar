c4d06129a19983252f35bc3a4e24beab46633088
Andreas Klintberg <ankl@kth.se>
2019-07-06 18:47:08 -0700

    updated package.xml

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=['setuptools', 'opendbc'],
     author='Faraz Khan',
     author_email='farazrkhan@gmail.com',
-    description='Densor Radar driver.',
+    description='Denso Radar driver.',
     license='MIT',
     entry_points={
         'console_scripts': [
